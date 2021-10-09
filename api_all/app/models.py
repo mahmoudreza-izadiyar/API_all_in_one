22b61b8ad24252740ab3703b7d57d227d536dc11
Mahmoudreza-Izadiyar <m.izadiyar@gmail.com>
2021-10-09 18:36:52 +1100

    admin, create a model, maitained model interface

--- api_all/app/models.py
+++ api_all/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    date_of_exp = models.DateField()
+    available = models.BooleanField()
+
+    def __str__(self):
+        return self.title
